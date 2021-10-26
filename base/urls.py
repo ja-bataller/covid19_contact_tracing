88b406060ad37073bd6bcefd6d63605bd9c358c6
John Anthony Bataller <johnanthonybataller.ce@gmail.com>
2021-10-26 22:31:34 +0800

    update template

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,18 @@ from . import views
 urlpatterns = [
     path('', views.index, name ="index"),
     path('scan/', views.scan, name="scan"),
-     path('login/', views.login_signup, name="login"),
+    path('login/', views.login_signup, name="login"),
+
+    # ADMINS URLS
+    path('admin_home/', views.admin_home, name="login"),
+    path('admin_user_info/', views.admin_user_info, name="admin_user_info"),
+    path('admin_active/', views.admin_active_dashboard, name="admin_active"),
+    path('admin_inactive/', views.admin_inactive_dashboard, name="admin_inactive"),
+    path('admin_pui/', views.admin_pui_dashboard, name="admin_pui"),
+    path('admin_about/', views.admin_about, name="admin_about"),
+
+    # CLIENT URLS
+    path('client_dashboard/', views.client_dashboard, name="client_dashboard"),
+    path('client_contact/', views.client_contact, name="client_contact"),
+    path('client_about/', views.client_about, name="client_about"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 def index(request):
     return render(request, 'index.html')
 
@@ -10,3 +8,35 @@ def scan(request):
 
 def login_signup(request):
      return render(request, 'login.html')
+
+# ADMINS VIEW
+
+def admin_home(request):
+     return render(request, 'admin_home.html')
+
+def admin_user_info(request):
+     return render(request, 'admin_user_info.html')
+
+def admin_active_dashboard(request):
+     return render(request, 'admin_active_dashboard.html')
+
+def admin_inactive_dashboard(request):
+     return render(request, 'admin_inactive_dashboard.html')
+
+def admin_pui_dashboard(request):
+     return render(request, 'admin_pui_dashboard.html')
+
+def admin_about(request):
+     return render(request, 'admin_about.html')
+
+
+# CLIENT VIEW
+
+def client_dashboard(request):
+     return render(request, 'client_dashboard.html')
+
+def client_contact(request):
+     return render(request, 'client_contact.html')
+
+def client_about(request):
+     return render(request, 'client_about.html')
