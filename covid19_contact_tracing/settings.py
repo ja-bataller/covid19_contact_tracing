cb4fc2b8f7c784dcb0cfae5f148ae86f03020c0d
John Anthony Bataller <johnanthonybataller.ce@gmail.com>
2022-01-09 18:34:33 +0800

    fixing statics

--- covid19_contact_tracing/settings.py
+++ covid19_contact_tracing/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-4k)xahb!4cqxbfb@epu1z-5j%!70a&o$us1-n+ig2kc(rwr3@w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://xm-malls-covid19-tracing.herokuapp.com/", "http://127.0.0.1/"]
+ALLOWED_HOSTS = ["xm-malls-covid19-tracing.herokuapp.com", "http://127.0.0.1/"]
 
 
 # Application definition
