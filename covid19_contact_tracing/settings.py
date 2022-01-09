621dbffb8d3dcd02f7a941aaf51cf65f09435195
John Anthony Bataller <johnanthonybataller.ce@gmail.com>
2022-01-09 19:57:11 +0800

    fixing deployment

--- covid19_contact_tracing/settings.py
+++ covid19_contact_tracing/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-import django_heroku
-
-
 import os
 ROOT_PATH = os.path.dirname(__file__)
 
@@ -48,8 +45,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -152,6 +149,3 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# # Activate Django-Heroku.
-# django_heroku.settings(locals())
