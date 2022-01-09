69d6f17a8fd2f5fae3a32e27e8cf9f4897bb547e
John Anthony Bataller <johnanthonybataller.ce@gmail.com>
2022-01-09 18:32:49 +0800

    fixing statics

--- covid19_contact_tracing/settings.py
+++ covid19_contact_tracing/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 
 import os
 ROOT_PATH = os.path.dirname(__file__)
@@ -150,5 +149,3 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
