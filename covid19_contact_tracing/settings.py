91a57f173a629d8daa31b54a1dec60f115f0a63f
John Anthony Bataller <johnanthonybataller.ce@gmail.com>
2022-01-09 19:32:50 +0800

    fixing deployment

--- covid19_contact_tracing/settings.py
+++ covid19_contact_tracing/settings.py
@@ -50,8 +50,8 @@ MIDDLEWARE = [
     # 'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # 'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
