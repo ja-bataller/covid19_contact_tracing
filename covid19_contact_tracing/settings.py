26d74a23f548a7a6eccf18adf718be7d8a42bb9d
John Anthony Bataller <johnanthonybataller.ce@gmail.com>
2022-01-09 16:53:54 +0800

    refactor req

--- covid19_contact_tracing/settings.py
+++ covid19_contact_tracing/settings.py
@@ -141,9 +141,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(ROOT_PATH, 'static')]
-
-STATICFILES_DIRS = [BASE_DIR, 'static']
+STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
