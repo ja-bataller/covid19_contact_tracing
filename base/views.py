b7306c0a9cdb66d3c7a0346986098337699cecd1
John Anthony Bataller <johnanthonybataller.ce@gmail.com>
2022-01-09 21:57:34 +0800

    refactor codes

--- base/views.py
+++ base/views.py
@@ -13,7 +13,7 @@ from .forms import SignUpForm
 
 import datetime
 
-from asgiref.sync import sync_to_async
+from asgiref.sync import async_to_sync
 
 def index(request):
     return render(request, 'index.html')
@@ -84,7 +84,7 @@ async def login_page(request):
         password = request.POST.get('password')
 
         try:
-            user = await sync_to_async (User.objects.get(username=contact_number), thread_sensitive=True)
+            user = await async_to_sync (User.objects.get(username=contact_number), thread_sensitive=True)
         except:
             context = {'page': page, 'response': "not_found"}
             return render(request, 'login.html', context)
