5f5878b15eccf80c9efb6ece2eea679266efdaa0
John Anthony Bataller <johnanthonybataller.ce@gmail.com>
2022-01-10 11:01:23 +0800

    still fixing the request db

--- covid19_contact_tracing/settings.py
+++ covid19_contact_tracing/settings.py
@@ -163,7 +163,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
-
 # Activate Django-Heroku.
 django_heroku.settings(locals())
